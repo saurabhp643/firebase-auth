835a477d8899017291446f638d491544f3576e24
saurabhp643 <saurabhp643@gmail.com>
2021-10-01 12:54:21 +0530

    okk auth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import { login, logout, selectUser } from "./features/userSlice";
-import {auth,googleProvider,facebookProvider } from "./firebase";
+import { auth, facebookProvider, googleProvider } from "./firebase";
 import Home from "./Home";
-import Login from "./Login";
 import google from "./img/google.png";
 import facebook from "./img/facebook.png";
-import twitter from "./img/twitter.png";
 import github from "./img/github.png";
+import twitter from "./img/twitter.png";
 
 function App() {
   const user = useSelector(selectUser);
@@ -16,25 +15,25 @@ function App() {
   const [isRegister, setIsRegister] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoginState, setIsLoginState]= useState("login");
+  const [isLoginState, setIsLoginState] = useState("login");
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    isRegister ? handleLogin():handleRegister();
+    isRegister ? handleLogin() : handleRegister();
   };
 
-   useEffect(() => {
+  useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         dispatch(
           login({
             id: authUser.uid,
             name: authUser.displayName ? authUser.displayName : authUser.email,
-            lastsignIn:authUser.metadata.lastSignInTime,
+            lastsignIn: authUser.metadata.lastSignInTime,
             verified: String(authUser.emailVerified),
             pic: authUser.photoURL
               ? authUser.photoURL
-              : "https://johannesippen.com/img/blog/humans-not-users/header.jpg",
+              : "https://lh3.googleusercontent.com/ogw/ADea4I5bHBJbpIvco4Yh1ARth7_gu4dl_QnpyDAU0NW8=s32-c-mo",
           })
         );
       } else {
@@ -47,35 +46,33 @@ function App() {
     if (email && password !== "") {
       auth
         .signInWithEmailAndPassword(email, password)
-        .then((data) => alert("Logged in successfully!!! 😆"))
+        .then((data) => alert("Logged in successfully!!!"))
         .catch((err) => alert(err));
     }
   };
-const handleRegister = () => {
+  const handleRegister = () => {
     if (email && password !== "") {
       auth
         .createUserWithEmailAndPassword(email, password)
-        .then((data) => alert("Logged in successfully!!! 😆"))
+        .then((data) => alert("Registered Successfully"))
         .catch((err) => alert(err));
     }
-      };
-  
-      const handleGoogle = ()=>{
+  };
+
+  const handleGoogle = () => {
     auth.signInWithPopup(googleProvider);
   };
-  const handleFacebook=()=>{
+  const handleFacebook = () => {
     auth.signInWithPopup(facebookProvider);
   };
-return (
+  return (
     <div className="App">
+      {" "}
       {user ? (
         <Home />
-       
       ) : (
         <>
           <div className="container">
-            
-      
             <div className="container-heading">
               <h1>
                 Ultimate Authentication using <br />
@@ -104,7 +101,6 @@ return (
                   required={true}
                   placeholder="Enter your email"
                 />
-                 
                 <input
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -112,33 +108,43 @@ return (
                   required={true}
                   placeholder="Enter your password"
                 />
-
-               
                 <button onClick={handleSubmit} type="submit">
                   Done
                 </button>
-                
+                {isLoginState === "login" && (
+                  <>
+                    <div className="providers">
+                      <div onClick={handleGoogle} className="provider">
+                        <img src={google} alt="google" />
+                      </div>
+                      <div onClick={handleFacebook} className="provider">
+                        <img src={facebook} alt="google" />
+                      </div>
+                      <div className="provider">
+                        <img src={github} alt="google" />
+                      </div>
+                      <div className="provider">
+                        <img src={twitter} alt="google" />
+                      </div>
+                    </div>
+                  </>
+                )}
                 <p>
-                  { 
-isRegister ? "New member? " : "Already registered? "}
-                 
+                  {isRegister ? "New member? " : "Already registered? "}
+
                   <span onClick={() => setIsRegister((show) => !show)}>
                     {isRegister ? "Register" : "Login"}
                   </span>
-                </p>
-              </div>
-              <Login />
-              
-            </div>
-            
-          </div>
-          
+                   
 
+                </p>
+              
+              </div>
+            </div>
+          </div>
         </>
       )}
-      
     </div>
-      
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
 import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLZv0uZxUh6_4OCKt7i2JtHmivyyMZQCg",
-  authDomain: "auth-61124.firebaseapp.com",
-  projectId: "auth-61124",
-  storageBucket: "auth-61124.appspot.com",
-  messagingSenderId: "462003553443",
-  appId: "1:462003553443:web:2a55dc276ec4f8fa0c6b16",
-  measurementId: "G-08BJ2YDN1P"
+  apiKey: "AIzaSyC8UHMG2PROL8H3x-uU1bU1Mp2DQn0uiEw",
+  authDomain: "auth-746a5.firebaseapp.com",
+  projectId: "auth-746a5",
+  storageBucket: "auth-746a5.appspot.com",
+  messagingSenderId: "1013640274798",
+  appId: "1:1013640274798:web:d362bde766c1e3e7d4d77f",
+  measurementId: "G-5639MW0HFN",
 };
+
 const firebaseapp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 const facebookProvider = new firebase.auth.FacebookAuthProvider();
-export { auth,googleProvider,facebookProvider};
 
+export { auth, googleProvider, facebookProvider };
